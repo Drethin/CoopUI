7954e685faa12d486a2a7c0302df85bb230e91a8
Drethin <a.hong@live.ca>
2016-07-26 23:22:02 -0400

    Test Commit

--- src/Client.java
+++ src/Client.java
@@ -3,6 +3,7 @@ import java.awt.*;
 
 /**
  * Created by Alex on 26/07/2016.
+ * TEST
  */
 class Client {
     public static void main(String args[]) {
@@ -26,7 +27,7 @@ class Client {
             public void run() {
                 ClientView view = new ClientView();
                 view.setSize(800, 600);
-                view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                view.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
                 view.setLocationRelativeTo(null);
                 view.setVisible(true);
 
